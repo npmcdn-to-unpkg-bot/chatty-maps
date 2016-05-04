6c0ec4bf44e9ad77018873fe00046bffdfdc8488
Micah Stubbs <micah.stubbs@gmail.com>
2016-05-04 09:23:50 -0700

    add robinson projection and determine current language based on currentVoice sent to responsiveVoice API

---
+++ robinson/02/console.js
@@ -0,0 +1,20 @@
+var logOfConsole = [];
+
+var _log = console.log,
+    _warn = console.warn,
+    _error = console.error;
+
+console.log = function() {
+    logOfConsole.push({method: 'log', arguments: arguments});
+    return _log.apply(console, arguments);
+};
+
+console.warn = function() {
+    logOfConsole.push({method: 'warn', arguments: arguments});
+    return _warn.apply(console, arguments);
+};
+
+console.error = function() {
+    logOfConsole.push({method: 'error', arguments: arguments});
+    return _error.apply(console, arguments);
+};
